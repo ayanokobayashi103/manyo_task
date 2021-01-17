671a7555d3d8588191c7e80407e144796832bbdb
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-01-17 12:35:47 +0900

    Readme編集herokuデプロイ手順

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
+# Task_app
+
+
+* Ruby version
+
+  -Ruby '2.6.5'
+
+  -Rails '5.2.4'
+
+  -PostgreSQL
+
+* Heroku deploy
+
+  `$ rails assets:precompile RAILS_ENV=production`
+
+  `$ git add -A`
+
+  `$ git commit -m "init"`
+
+  `$ heroku create`
+
+  `$ git push heroku master`
+
+  `$ heroku run rails db:migrate`
+
+* Table
+
 | user     | label   | task    |
 | -------- | ------- | ------- |
 | id       | id      | id      |
 | name     | user_id | task_id |
 | email    |         | title   |
-| passward |         | content | 
+| passward |         | content |
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :basic_auth, if: :production?
   before_action :set_task, only: [:edit, :show, :update, :destroy]
 
   def index
@@ -40,6 +41,16 @@ class TasksController < ApplicationController
   end
 
   private
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
   def task_params
     params.require(:task).permit(:title, :content)
   end
