b6f962777d76955396f976ec5545cbd3f5d832ee
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-01-22 18:09:33 +0900

    ログイン機能追加、アソシエーション

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: "新規登録しました！"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      if @user.update
+        redirect_to
+      else
+        render :edit
+      end
+    end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
