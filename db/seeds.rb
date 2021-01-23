d31c7ba1c3f3a298c4cd056d32c5d9d1cac0d234
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-01-23 21:47:41 +0900

    ユーザ管理画面追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(
+  name:  "管理者たろう",
   email: 'test@test.com',
-  name: 'テスト太郎',
   password: 'testarou',
   password_confirmation: 'testarou',
+  admin: true
 )
