2746ed3dbe1360d1b8a10d99ae220bf3703fbc2b
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-01-24 17:17:22 +0900

    管理者人数の削除制御

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -13,8 +13,7 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to user_path(@user.id), notice: "新規登録しました！"
+      redirect_to admin_users_path(@user.id), notice: "新規登録しました！"
     else
       render :new
     end
@@ -37,7 +36,11 @@ class Admin::UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    redirect_to admin_users_path, notice:"削除しました"
+    if @user.admin?
+      redirect_to admin_users_path, notice:"管理者が0人になるので削除できません"
+    else
+      redirect_to admin_users_path, notice:"削除しました"
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-#  before_destroy :admin_destroy
+  before_destroy :admin_destroy
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
@@ -8,9 +8,15 @@ class User < ApplicationRecord
   validates :password, presence: true, length:{ minimum: 6 }
   has_many :tasks, dependent: :destroy
 
-#  private
-#  def admin_destroy
-  #  unless admin <= 1
-  #  end
-#  end
+  private
+  def admin_destroy
+    if admin?
+      admin=User.where(admin:true)
+      if admin.count > 0
+        # errors.add(:admin, ": 管理者がいなくなってしまいます")
+        # returnと言う意味
+        throw :abort
+      end
+    end
+  end
 end
