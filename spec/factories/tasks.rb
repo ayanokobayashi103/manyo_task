469d71b1dc35ade83f1338bfc39ca340860f864f
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-01-27 15:34:28 +0900

    Task Systemテスト修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,13 +2,16 @@ FactoryBot.define do
   factory :task do
     title { 'Factoryで作ったデフォルトのタイトル１' }
     content { 'Factoryで作ったデフォルトのコンテント１' }
+    user
   end
   factory :second_task, class: Task do
     title { 'Factoryで作ったデフォルトのタイトルnewest' }
     content { 'Factoryで作ったデフォルトのコンテントnewest' }
+    user
   end
   factory :third_task, class: Task do
     title { 'Factoryで作ったデフォルトのタイトル3' }
     content { 'Factoryで作ったデフォルトのコンテント3' }
+    user
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 RSpec.describe 'Task', type: :system do
+  before do
+    FactoryBot.create(:admin_user)
+    user1 = FactoryBot.create(:user)
+    visit new_session_path
+    fill_in 'session[email]', with:'user@u.com'
+    fill_in 'session[password]', with:'userpass1'
+    click_on 'Log in'
+    FactoryBot.create(:task, title: 'task', status: 2, priority: 1,user: user1)
+    FactoryBot.create(:second_task, title: 'sample', deadline: Time.current + 3.days, priority: 3,user: user1)
+  end
   describe '検索機能' do
     before do
-      FactoryBot.create(:task, title: "task", status: 2)
-      FactoryBot.create(:second_task, title: "sample")
       visit tasks_path
     end
     context 'タイトルであいまい検索をした場合' do
@@ -29,6 +37,7 @@ RSpec.describe 'Task', type: :system do
       end
     end
   end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -42,7 +51,7 @@ RSpec.describe 'Task', type: :system do
   end
 
   describe '一覧表示機能' do
-    let!(:task) { FactoryBot.create(:task, title: 'task', priority: 1) }
+
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         visit tasks_path
@@ -51,10 +60,9 @@ RSpec.describe 'Task', type: :system do
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        FactoryBot.create(:second_task)
         visit tasks_path
         click_on '作成日時でソートする'
-        sleep(2)
+        sleep(1)
         task_list = all('tr')
         latest = task_list[1]
         expect(latest).to have_content 'newest'
@@ -62,10 +70,9 @@ RSpec.describe 'Task', type: :system do
     end
     context 'タスクが終了期限の降順に並んでいる場合' do
       it '終了期限が近いものが一番上に表示される' do
-        FactoryBot.create(:second_task, deadline: Time.current + 3.days)
         visit tasks_path
         click_on '終了期限でソートする'
-        sleep(2)
+        sleep(1)
         task_list = all('tr')
         closest_deadline = task_list[1]
         expect(closest_deadline).to have_content 'task'
@@ -73,10 +80,9 @@ RSpec.describe 'Task', type: :system do
     end
     context '優先順位が高い順に並んでいる場合' do
       it '優先度の高が一番上に表示される' do
-        FactoryBot.create(:second_task, priority: 3)
         visit tasks_path
         click_on '優先度'
-        sleep(2)
+        sleep(1)
         priority = all('tr')
         high_priority = priority[1]
         expect(high_priority).to have_content '高'
@@ -86,9 +92,9 @@ RSpec.describe 'Task', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        task_a = FactoryBot.create(:task, title: 'task_a')
-        visit task_path(task_a)
-        expect(page).to have_content 'task_a'
+        visit tasks_path
+        page.all(".btn btn-warning btn-xs")[1].click
+        expect(page).to have_content 'sample'
       end
     end
   end
